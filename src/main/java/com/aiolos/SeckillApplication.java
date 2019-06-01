a27308a2b9823770a8e5f1e2871068a384146d09
aioloscn <756243011@qq.com>
2019-06-01 17:26:36 +0800

    'initialize'

---
+++ src/main/java/com/aiolos/SeckillApplication.java
@@ -0,0 +1,17 @@
+package com.aiolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Aiolos
+ * @date 2019-06-01 15:40
+ */
+@EnableAutoConfiguration
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+}
