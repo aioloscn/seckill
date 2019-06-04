2e277507f665210268bb72e72a20240df1bf0a94
aiolos <15988346908@163.com>
2019-06-04 23:40:44 +0800

    generate mybatis

--- src/main/java/com/aiolos/SeckillApplication.java
+++ src/main/java/com/aiolos/SeckillApplication.java
@@ -1,15 +1,34 @@
 package com.aiolos;
 
+import com.aiolos.seckill.dao.UserDOMapper;
+import com.aiolos.seckill.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Aiolos
  * @date 2019-06-01 15:40
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.aiolos"})
+@MapperScan("com.aiolos.seckill.dao")
+@RestController
 public class SeckillApplication {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @GetMapping
+    public String query() {
+
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null)
+            return "null";
+        return "ok";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SeckillApplication.class, args);
